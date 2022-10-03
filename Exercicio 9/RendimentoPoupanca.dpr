7b108eb95a8cc858a110f552744cf52bfa8f087a
FURB\nagoncalves <nathan.andrade1504@gmail.com>
2022-10-03 20:29:32 -0300

    Rendimento de poupança.

---
+++ Exercicio 9/RendimentoPoupanca.dpr
@@ -0,0 +1,14 @@
+program RendimentoPoupanca;
+
+uses
+  Vcl.Forms,
+  uFrmJuros in 'uFrmJuros.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
