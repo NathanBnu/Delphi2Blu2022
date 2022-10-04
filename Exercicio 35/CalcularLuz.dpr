e3307c9e7b38408b41b4b357cfec39f99277dcfa
FURB\nagoncalves <nathan.andrade1504@gmail.com>
2022-10-03 21:53:46 -0300

    Sistema para calcular conta de luz.

---
+++ Exercicio 35/CalcularLuz.dpr
@@ -0,0 +1,14 @@
+program CalcularLuz;
+
+uses
+  Vcl.Forms,
+  uFrmContaLuz in 'uFrmContaLuz.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
