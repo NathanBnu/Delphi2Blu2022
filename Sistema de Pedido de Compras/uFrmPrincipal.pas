e43a2d65e1c5a6f04ec71ab46cc043f296066722
FURB\nagoncalves <nathan.andrade1504@gmail.com>
2022-10-31 20:38:13 -0300

    Sistema de Pedido de Compras

---
+++ Sistema de Pedido de Compras/uFrmPrincipal.pas
@@ -0,0 +1,38 @@
+unit uFrmPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
+
+type
+  TfrmPrincipal = class(TForm)
+    Panel1: TPanel;
+    Image1: TImage;
+    Label1: TLabel;
+    Image4: TImage;
+    Image3: TImage;
+    Image2: TImage;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Image5: TImage;
+    Label5: TLabel;
+    Panel2: TPanel;
+    Image6: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
