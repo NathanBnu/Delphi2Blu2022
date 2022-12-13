d9a11e3f8f11afe4be2de11aa953a69c5973cb39
FURB\mysql <nathan-andrade17@outlook.com>
2022-12-12 21:57:53 -0300

    Netflix BackEnd!

--- Mobile(FireMonkey)/Netflix/uFrmPrincipal.pas
+++ Mobile(FireMonkey)/Netflix/uFrmPrincipal.pas
@@ -33,7 +33,9 @@ type
     ListBox1: TListBox;
     FloatAnimation1: TFloatAnimation;
   private
-    { Private declarations }
+    procedure LoadMenu;
+    procedure OpenMenu(ind: Boolean);
+    procedure SetupMenu(Item: TListBoxItem; texto: String);
   public
     { Public declarations }
   end;
@@ -44,5 +46,58 @@ var
 implementation
 
 {$R *.fmx}
+
+{ TfrmPrincipal }
+
+procedure TfrmPrincipal.LoadMenu;
+begin
+  ListBox1.Items.Clear;
+
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Todos os gêneros');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Disponível para download');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Ação');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Adolescente');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Anime');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Asiático');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Brasileiro');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Comédia');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Drama');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Esporte');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Mistérios');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Novelas');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Suspense');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Terror');
+  Self.SetupMenu(TListBoxItem.Create(ListBox1), 'Novela');
+end;
+
+procedure TfrmPrincipal.OpenMenu(ind: Boolean);
+begin
+  //Esconde o item selecionado...
+  ListBox1.ItemIndex := -1;
+
+  //Volta a listagem para o inicio
+  ListBox1.ScrollToItem(ListBox1.ItemByIndex(0));
+
+  if ind then
+  begin
+    lytMenu.Visible := True;
+    lytMenu.Tag := 1;
+    lytGenero.AnimateFloat('Opacity', 0, 0.2);
+    FloatAnimation1.Inverse := False;
+  end
+  else
+  begin
+    lytMenu.Tag := 0;
+    lytGenero.AnimateFloat('Opacity', 1, 0.4);
+    FloatAnimation1.Inverse := True;
+  end;
+
+  FloatAnimation1.Start;
+end;
+
+procedure TfrmPrincipal.SetupMenu(Item: TListBoxItem; texto: String);
+begin
+
+end;
 
 end.
