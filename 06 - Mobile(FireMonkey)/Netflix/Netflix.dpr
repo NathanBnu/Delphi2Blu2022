979f307fe8d54b48a1bbb9ed30bc7cccf6ed33f5
NATHAN\nathan <nathan.andrade1504@gmail.com>
2022-12-13 12:24:02 -0300

    Netflix em Mobile.

---
+++ 06 - Mobile(FireMonkey)/Netflix/Netflix.dpr
@@ -0,0 +1,14 @@
+program Netflix;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uFrmPrincipal in 'uFrmPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
