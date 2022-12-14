d3bf9dcd70ac96d52b08a8e19217a2d1c9f8e2c5
FURB\mysql <nathan-andrade17@outlook.com>
2022-12-13 21:56:01 -0300

    MercadoLivre Mobile.

---
+++ 06 - Mobile(FireMonkey)/MercadoLivre/uFrmPrincipal.pas
@@ -0,0 +1,55 @@
+unit uFrmPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TfrmPrincipal = class(TForm)
+    VertScrollBox1: TVertScrollBox;
+    rect_Valores: TRectangle;
+    lblTitulo: TLabel;
+    lytProduto: TLayout;
+    lblTituloProduto: TLabel;
+    lblCentavosProduto: TLabel;
+    lblReaisProduto: TLabel;
+    lytEnvio: TLayout;
+    lblTituloEnvio: TLabel;
+    lblCentavosEnvio: TLabel;
+    lblReaisEnvio: TLabel;
+    linhaEnvio: TLine;
+    lytParcelas: TLayout;
+    lblTituloParcelas: TLabel;
+    lblCentavosParcelas: TLabel;
+    lblReaisParcelas: TLabel;
+    lblParcelasParcelas: TLabel;
+    lyt_botao_compra: TLayout;
+    Rectangle1: TRectangle;
+    btn_comprar: TSpeedButton;
+    lyt_compra_ok: TLayout;
+    Image1: TImage;
+    lyt_loading_compra: TLayout;
+    rect_barra: TRectangle;
+    Label1: TLabel;
+    rect_fundo: TRectangle;
+    rect_fundo_branco_top: TRectangle;
+    lyt_detalhes: TLayout;
+    Rectangle2: TRectangle;
+    circuloEndereco: TCircle;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.fmx}
+
+end.
