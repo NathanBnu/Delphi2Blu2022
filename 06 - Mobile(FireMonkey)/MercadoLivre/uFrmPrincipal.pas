a89d9a0af1bbee58852cfce274d3868ce72db0c0
FURB\mysql <nathan-andrade17@outlook.com>
2022-12-14 21:56:36 -0300

    MercadoLivre BackEnd.

--- 06 - Mobile(FireMonkey)/MercadoLivre/uFrmPrincipal.pas
+++ 06 - Mobile(FireMonkey)/MercadoLivre/uFrmPrincipal.pas
@@ -37,7 +37,7 @@ type
     rect_fundo: TRectangle;
     rect_fundo_branco_top: TRectangle;
     lyt_detalhes: TLayout;
-    Rectangle2: TRectangle;
+    rect_fundo_branco_bottom: TRectangle;
     circuloEndereco: TCircle;
     lblproduto: TLabel;
     lblQuantidade: TLabel;
@@ -46,8 +46,13 @@ type
     lblEndereco: TLabel;
     lblComplemento: TLabel;
     lblComprador: TLabel;
+    procedure FormShow(Sender: TObject);
+    procedure btn_comprarClick(Sender: TObject);
   private
-    { Private declarations }
+    FLoop: String;
+
+    procedure LoadingCompra(aInd: Boolean);
+    procedure Comprar_WS;
   public
     { Public declarations }
   end;
@@ -58,5 +63,96 @@ var
 implementation
 
 {$R *.fmx}
+
+{ TfrmPrincipal }
+
+procedure TfrmPrincipal.btn_comprarClick(Sender: TObject);
+begin
+  Self.LoadingCompra(True);
+  rect_barra.Margins.Right := rect_fundo.Width;
+  FLoop := 'S';
+
+  Self.Comprar_WS;
+
+  TThread.CreateAnonymousThread(procedure
+  begin
+    while FLoop = 'S' do
+    begin
+      if rect_barra.Margins.Right <= 0 then
+        rect_barra.Margins.Right := rect_fundo.Width;
+
+        Sleep(150);
+
+        TThread.Synchronize(nil, procedure
+        begin
+          rect_barra.Margins.Right := rect_barra.Margins.Right - 1;
+        end);
+    end;
+
+    //Finalizou com erro...
+    if FLoop = 'ERRO' then
+    begin
+      TThread.Synchronize(nil, procedure
+      begin
+        Self.LoadingCompra(False);
+        ShowMessage('Erro ao finalizar compra');
+      end);
+    end;
+
+    //Finalizou com sucesso...
+    if FLoop = 'OK' then
+    begin
+      TThread.Synchronize(nil, procedure
+      begin
+        lyt_loading_compra.AnimateFloat('Margins.Left', 150, 0.3);
+        lyt_loading_compra.AnimateFloat('Margins.Right', 150, 0.3);
+        lyt_loading_compra.AnimateFloat('Opacity', 0, 0.3);
+      end);
+
+      Sleep(450);
+
+      TThread.Synchronize(nil, procedure
+      begin
+         lyt_compra_ok.Visible := True;
+         lyt_compra_ok.AnimateFloat('Opacity', 1, 0.4);
+      end);
+
+      Sleep(1000);
+
+      TThread.Synchronize(nil, procedure
+      begin
+        ShowMessage('Compra realizada com sucesso');
+      end);
+    end;
+  end).Start;
+end;
+
+procedure TfrmPrincipal.Comprar_WS;
+begin
+  TThread.CreateAnonymousThread(procedure
+  begin
+    Sleep(5000);
+    //...
+    //...
+    //...
+    FLoop := 'OK';
+  end).Start;
+end;
+
+procedure TfrmPrincipal.FormShow(Sender: TObject);
+begin
+  Self.LoadingCompra(False);
+end;
+
+procedure TfrmPrincipal.LoadingCompra(aInd: Boolean);
+begin
+  rect_fundo_branco_top.Visible := aInd;
+  rect_fundo_branco_bottom.Visible := aInd;
+
+  lyt_botao_compra.Visible := not aInd;
+  lyt_loading_compra.Visible := aInd;
+  lyt_compra_ok.Visible := False;
+  lyt_compra_ok.Opacity := 0;
+end;
 
 end.
