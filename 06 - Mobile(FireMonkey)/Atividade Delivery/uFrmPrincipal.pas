37a26a70cd9707e0f09a53e0816646517a54271b
NATHAN\nathan <nathan.andrade1504@gmail.com>
2022-12-19 16:18:12 -0300

    Delivery.

--- 06 - Mobile(FireMonkey)/Atividade Delivery/uFrmPrincipal.pas
+++ 06 - Mobile(FireMonkey)/Atividade Delivery/uFrmPrincipal.pas
@@ -9,19 +9,36 @@ uses
 
 type
   TfrmPrincipal = class(TForm)
-    FlowLayout1: TFlowLayout;
     ToolBar1: TToolBar;
     Rectangle1: TRectangle;
     Label1: TLabel;
-    Rectangle2: TRectangle;
     ShadowEffect1: TShadowEffect;
-    ShadowEffect2: TShadowEffect;
+    FlowLayout1: TFlowLayout;
+    Rectangle2: TRectangle;
     Image1: TImage;
     Label2: TLabel;
     Label3: TLabel;
-    Rectangle3: TRectangle;
+    ShadowEffect2: TShadowEffect;
     ShadowEffect3: TShadowEffect;
+    Rectangle4: TRectangle;
+    Image3: TImage;
+    Label5: TLabel;
+    Rectangle5: TRectangle;
+    Image4: TImage;
+    Label6: TLabel;
+    Label7: TLabel;
+    Rectangle3: TRectangle;
     Image2: TImage;
+    Label8: TLabel;
+    Label9: TLabel;
+    Rectangle6: TRectangle;
+    Image5: TImage;
+    Label10: TLabel;
+    Label11: TLabel;
+    Rectangle7: TRectangle;
+    Image6: TImage;
+    Label12: TLabel;
+    Label13: TLabel;
   private
     { Private declarations }
   public
