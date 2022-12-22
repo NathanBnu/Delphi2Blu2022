49333101b9f6f0fbf4602390fa8e9a64a48d3ae6
Nathan <nathan.andrade1504@gmail.com>
2022-12-21 21:06:12 -0300

    Atividade layout café!

--- 06 - Mobile(FireMonkey)/Atividade Coffee/uFrmPrincipal.pas
+++ 06 - Mobile(FireMonkey)/Atividade Coffee/uFrmPrincipal.pas
@@ -16,8 +16,8 @@ type
     rect_cesto: TRectangle;
     rect_barra_de_pesquisa: TRectangle;
     lblSearch: TLabel;
-    Rectangle3: TRectangle;
-    Image1: TImage;
+    rect_pesquisa: TRectangle;
+    imgPesquisa: TImage;
     rect_paginas: TRectangle;
     lblCoffee: TLabel;
     lblNonCoffee: TLabel;
@@ -32,6 +32,48 @@ type
     rect_fundo: TRectangle;
     Circle1: TCircle;
     lblMais: TLabel;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Label1: TLabel;
+    Label2: TLabel;
+    Circle2: TCircle;
+    Label3: TLabel;
+    Rectangle4: TRectangle;
+    Rectangle5: TRectangle;
+    Label4: TLabel;
+    Label5: TLabel;
+    Circle3: TCircle;
+    Label6: TLabel;
+    Rectangle6: TRectangle;
+    Rectangle7: TRectangle;
+    Label7: TLabel;
+    Label8: TLabel;
+    Circle4: TCircle;
+    Label9: TLabel;
+    Rectangle8: TRectangle;
+    Rectangle9: TRectangle;
+    Label10: TLabel;
+    Label11: TLabel;
+    Circle5: TCircle;
+    Label12: TLabel;
+    Rectangle10: TRectangle;
+    Rectangle11: TRectangle;
+    Label13: TLabel;
+    Label14: TLabel;
+    Circle6: TCircle;
+    Label15: TLabel;
+    rect_rodape: TRectangle;
+    Rectangle12: TRectangle;
+    lblHome: TLabel;
+    Image2: TImage;
+    Rectangle13: TRectangle;
+    Image3: TImage;
+    Image4: TImage;
+    Rectangle14: TRectangle;
+    Image5: TImage;
+    Rectangle15: TRectangle;
+    Image6: TImage;
+    procedure FlowLayout1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -43,6 +85,14 @@ var
 
 implementation
 
+uses
+  uFrmSecundaria;
+
 {$R *.fmx}
+
+procedure TfrmPrincipal.FlowLayout1Click(Sender: TObject);
+begin
+  frmSecundaria.show;
+end;
 
 end.
---
+++ 06 - Mobile(FireMonkey)/Atividade Coffee/uFrmSecundaria.pas
@@ -0,0 +1,58 @@
+unit uFrmSecundaria;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TfrmSecundaria = class(TForm)
+    ToolBar1: TToolBar;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Image1: TImage;
+    Rectangle3: TRectangle;
+    Image2: TImage;
+    rect_Titulo: TRectangle;
+    lblTitulo: TLabel;
+    rect_descri: TRectangle;
+    Label1: TLabel;
+    rect_image: TRectangle;
+    Rectangle6: TRectangle;
+    rect_tamanho: TRectangle;
+    Label2: TLabel;
+    Circle1: TCircle;
+    Label3: TLabel;
+    Circle3: TCircle;
+    Label5: TLabel;
+    Circle2: TCircle;
+    Label4: TLabel;
+    rect_quantidade: TRectangle;
+    Label6: TLabel;
+    Circle4: TCircle;
+    Label7: TLabel;
+    Circle6: TCircle;
+    Label9: TLabel;
+    Circle5: TCircle;
+    Label8: TLabel;
+    rect_preco: TRectangle;
+    Label10: TLabel;
+    Label12: TLabel;
+    Circle7: TCircle;
+    Image3: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmSecundaria: TfrmSecundaria;
+
+implementation
+
+{$R *.fmx}
+
+end.
