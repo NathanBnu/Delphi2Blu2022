086ad7f77fd7265a6b2548edc2cf64a43aebadd2
Nathan <nathan.andrade1504@gmail.com>
2023-02-07 21:28:02 -0300

    Utilizando o Swagger API.

---
+++ 10 - WebServer/JSON - Swegger/Swegger.dpr
@@ -0,0 +1,60 @@
+program Swegger;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  Horse,
+  Horse.CORS,
+  Horse.GBSwagger,
+  UPessoa in 'Model\entities\UPessoa.pas',
+  UController.Pessoa in 'Model\controllers\UController.Pessoa.pas';
+
+procedure SwaggerConfig;
+  begin
+    //isso aqui é programação funcional
+    //usa interface
+    Swagger
+      .Register
+        .SchemaOnError(TAPIError)
+        .&End
+        .Info
+          .Title('Minha Pirmeira API documentada')
+          .Description('API Horse')
+          .Contact
+            .Name('Armando Neto')
+            .Email('email@hotmail.com')
+            .URL('http://www.mypage.com.br')
+          .&End
+        .&End
+      .BasePath('v1');
+  end;
+
+  procedure Registry;
+  begin
+    //V1 - Versionamento da API
+    THorse.Group.Prefix('v1')
+      .Get('/pessoas', TPessoaController.GetPessoa)
+      .Get('/pessoas/:id', TPessoaController.FindUser)
+      .Post('/pessoas', TPessoaController.InsertUser)
+      .Put('/pessoas/:id', TPessoaController.UpdateUser)
+      .Delete('/pessoas/:id', TPessoaController.DeleteUser)
+  end;
+
+begin
+  THorse
+    .Use(CORS)
+    .Use(HorseSwagger);
+
+  //http://localhost:9000/swagger/doc/html
+  SwaggerConfig;
+  THorseGBSwaggerRegister.RegisterPath(TPessoaController);
+
+  Registry;
+
+  THorse.Listen(9000,
+    procedure(Horse: THorse)
+    begin
+      Writeln('Server is runing on port ' + IntToStr(Horse.Port));
+    end);
+end.
