397c83e327ff8c43423e2bf340ad051938189242
Nathan <nathan.andrade1504@gmail.com>
2023-02-15 20:45:41 -0300

    Criando as entidades do projeto.

---
+++ 11 - Projeto/4 - Projeto/BackEnd/DevsBets_Backend.dpr
@@ -0,0 +1,28 @@
+program DevsBets_Backend;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Horse,
+  System.SysUtils,
+  UEntity.Users in 'model\entities\UEntity.Users.pas',
+  UEntity.Logins in 'model\entities\UEntity.Logins.pas',
+  UEntity.Teams in 'model\entities\UEntity.Teams.pas',
+  UEntity.Matchs in 'model\entities\UEntity.Matchs.pas',
+  UEntity.Bets in 'model\entities\UEntity.Bets.pas',
+  UDAO.Intf in 'model\dao\UDAO.Intf.pas';
+
+begin
+
+  THorse.Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+  THorse.Listen(9000,
+    procedure(Horse: THorse)
+    begin
+      Writeln('Server is runing on port ' + IntToStr(Horse.Port));
+    end);
+end.
