5fe49de59f1471e1682e5bf3e84bf882abc678f5
Nathan <nathan.andrade1504@gmail.com>
2023-02-23 20:13:36 -0300

    Desenvolvendo a interface.

---
+++ 11 - Projeto/4 - Projeto/frontend - desktop/model/services/UService.Intf.pas
@@ -0,0 +1,16 @@
+unit UService.Intf;
+
+interface
+
+type
+  IService = interface
+    procedure CarregarToken;
+    procedure Registrar;
+    procedure Listar;
+    procedure Excluir;
+
+    function ObterRegistro(const aId: Integer): TObject;
+  end;
+implementation
+
+end.
